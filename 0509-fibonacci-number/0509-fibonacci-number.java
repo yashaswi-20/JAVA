3a81ade401bb1733564e6414dcfdd09ed3e406f9
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2025-12-05 10:50:19 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int filo(int n,int[]dp){
+     HashMap<Integer,Integer>map=new HashMap<>();
+    public int filo(int n){
         if(n==0)return 0;
         if(n<=2)return 1;
-        if(dp[n]!=-1)return dp[n];
-        dp[n]= filo(n-1,dp)+filo(n-2,dp);
-        return dp[n];
+        if(map.containsKey(n))return map.get(n);
+        map.put(n, filo(n-1)+filo(n-2));
+        return map.get(n);
     }
     public int fib(int n) {
-        int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-         return filo(n,dp);
+      
+       
+         return filo(n);
     }
 }
