da76146594f4016ebffbfd09b7da79140c6ada52
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2025-12-06 22:32:47 +0530

    Time: 6 ms (1.52%), Space: 43.2 MB (12.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,15 +16,15 @@ class Pair{
 }
 
 class Solution {
-    HashMap<Pair,Integer>map=new HashMap<>();
+    HashMap<String,Integer>map=new HashMap<>();
     public int uniquePaths(int m, int n) {
-        Pair key=new Pair(m,n);
-        if(map.containsKey(key))return map.get(key);
+        String s=m+","+n;
+        if(map.containsKey(s))return map.get(s);
         if(m==1 && n==1)return 1;
         if(m==0 || n==0)return 0;
         
-        int ways=uniquePaths(m-1,n)+uniquePaths(m,n-1);
-        map.put(key,ways);
-        return ways;
+        map.put(s,uniquePaths(m-1,n)+uniquePaths(m,n-1));
+       
+        return map.get(s);
     }
 }
