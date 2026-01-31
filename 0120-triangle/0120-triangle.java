7dbb6a44a3b6b59a9d620826461a2b2dc3d02fcb
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-01-31 19:02:17 +0530

    Time: 1 ms (99.78%), Space: 45.6 MB (60.75%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,32 +1,18 @@
 class Solution {
-
-    int compute(List<List<Integer>> arr, int r, int c, int lr, int[][] dp) {
-        // invalid index
-        if (c < 0 || c >= arr.get(r).size()) {
-            return Integer.MAX_VALUE;
-        }
-
-        // base case
-        if (r == lr - 1) {
-            return arr.get(r).get(c);
-        }
-
-        if (dp[r][c] != 12000) return dp[r][c];
-
-        int down = compute(arr, r + 1, c, lr, dp);
-        int diag = compute(arr, r + 1, c + 1, lr, dp);
-
-        return dp[r][c] = arr.get(r).get(c) + Math.min(down, diag);
+    int compute(List<List<Integer>>arr,int r,int c,int lr,int dp[][]){
+        if(r==lr-1)return arr.get(r).get(c);
+        if(c>arr.get(r).size()-1)return 0;
+        if(dp[r][c]!=12000)return dp[r][c];
+        int same=compute(arr,r+1,c,lr,dp)+arr.get(r).get(c);
+        int next=compute(arr,r+1,c+1,lr,dp)+arr.get(r).get(c);
+        return dp[r][c]=Math.min(same,next);
     }
-
     public int minimumTotal(List<List<Integer>> arr) {
-        int lr = arr.size();
-        int[][] dp = new int[lr][lr];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, 12000);
+        int lr=arr.size();
+        int dp[][]=new int[lr][lr];
+        for(int [] a:dp){
+            Arrays.fill(a,12000);
         }
-
-        return compute(arr, 0, 0, lr, dp);
+        return compute(arr,0,0,lr,dp);
     }
 }
