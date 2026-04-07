83196be1320e458a3ef417b62d35e961c68faa1b
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-07 12:02:39 +0530

    Time: 6 ms (99.09%), Space: 46.3 MB (43.45%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -8,14 +8,13 @@ class Solution {
         int i=0;
         while(j<n){
             freq[s.charAt(j)-'A']++;
-            for(int p=0;p<26;p++){
-                maxfreq=Math.max(maxfreq,freq[p]);
-            }
+            maxfreq=Math.max(maxfreq,freq[s.charAt(j)-'A']);
             if((j-i+1)-maxfreq<=k){
                 maxlen=Math.max(maxlen,j-i+1); 
             }else{
                 while( (j-i+1)-maxfreq>k){
                     freq[s.charAt(i)-'A']--;
+                    //for(int p=0;p<26;p++){ maxfreq=Math.max(maxfreq,freq[p]); }
                     i++;
                 }
             }
