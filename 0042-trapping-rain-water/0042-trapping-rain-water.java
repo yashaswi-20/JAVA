ed7c05f04eb40406975ebbedc4f891d7e06af881
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-08 17:28:09 +0530

    Time: 1 ms (58.81%), Space: 47.5 MB (85.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,17 +2,22 @@ class Solution {
     public int trap(int[] height) {
             int maxVolume=0;
             int leftmax=0;
+            int rmax[]=new int[height.length];
+            int rvalue=0;
+            for(int i=height.length-1;i>=0;i--){
+                rvalue=Math.max(rvalue,height[i]);
+                rmax[i]=rvalue;
+                //System.out.println(rmax[i]);
+            }
+            int rightmax=0;         
         for(int i=0;i<height.length;i++){
             int left=0;
             int right=height.length-1;
-            int rightmax=0;
-            
                 leftmax=Math.max(leftmax,height[i]);
-                System.out.println(leftmax);
-            while(right>=i){
-                rightmax=Math.max(rightmax,height[right]);
-                right--;
-            }
+            
+                rightmax=rmax[i];
+                
+            
             int vol=Math.min(leftmax,rightmax)-height[i];
             maxVolume+=vol;
         }
