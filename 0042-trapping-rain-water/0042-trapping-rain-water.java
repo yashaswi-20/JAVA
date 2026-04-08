5a8e00da70a905f1f04b2e2d3017b6c213310a7a
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-08 17:31:59 +0530

    Time: 1 ms (58.81%), Space: 47.5 MB (85.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -7,17 +7,11 @@ class Solution {
             for(int i=height.length-1;i>=0;i--){
                 rvalue=Math.max(rvalue,height[i]);
                 rmax[i]=rvalue;
-                //System.out.println(rmax[i]);
             }
             int rightmax=0;         
         for(int i=0;i<height.length;i++){
-            int left=0;
-            int right=height.length-1;
                 leftmax=Math.max(leftmax,height[i]);
-            
                 rightmax=rmax[i];
-                
-            
             int vol=Math.min(leftmax,rightmax)-height[i];
             maxVolume+=vol;
         }
