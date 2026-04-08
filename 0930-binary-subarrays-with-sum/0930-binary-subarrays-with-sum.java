33d6bf9b054d998008996bbc376e3fcbdf9042d9
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-08 23:34:54 +0530

    Time: 21 ms (46.23%), Space: 50.6 MB (90.99%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,32 +1,13 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
        HashMap<Integer,Integer>map=new HashMap<>();
-       int prefix[]=new int[nums.length];
        int val=0;
+       int res=0;
+       map.put(0,1);
        for(int i=0;i<nums.length;i++){
             val+=nums[i];
-            prefix[i]=val;
-       }
-       int res=0;
-       for(int i=0;i<prefix.length;i++){
-        
-        if(prefix[i]==goal){
-            int add=map.getOrDefault(0,0);
-             map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
-            res+=add+1;
-            //System.out.println('a');
-        }
-        else if(prefix[i]>goal){
-            int check=prefix[i]-goal;
-            int add=map.getOrDefault(check,0);
-             map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
-           // System.out.println('b');
-            res+=add;
-        }else{
-           // System.out.println('c');
-
-            map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
-        }
+            res+=map.getOrDefault(val-goal,0);
+            map.put(val,map.getOrDefault(val,0)+1);
        }
         return res;
     }
