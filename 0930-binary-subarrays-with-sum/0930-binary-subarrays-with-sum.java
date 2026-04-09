f02978df7391f99038dc4d38e109b7f5bbd03c00
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-09 21:28:27 +0530

    Time: 1 ms (100%), Space: 50.9 MB (58.87%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,32 +1,21 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        int left=0;
+
+    int atMost(int []nums,int goal){
+        if(goal<0)return 0;
         int sum=0;
-        int result=0;
-       for(int right=0;right<nums.length;right++){
+        int left=0;
+        int res=0;
+        for(int right=0;right<nums.length;right++){
             sum+=nums[right];
-                while(sum>goal){
+            while(sum>goal){
                 sum-=nums[left];
                 left++;
             }
-            result+=right-left+1;
-       }
-       int left1=0;
-        int sum1=0;
-        int result1=0;
-        for(int right=0;right<nums.length;right++){
-            if(goal<=0){
-                result1=0;
-                break;
-            }
-            sum1+=nums[right];
-                while(sum1>goal-1){
-                sum1-=nums[left1];
-                left1++;
-            }
-            result1+=right-left1+1;
-       }
-       return result-result1;
-
+            res+=right-left+1;
+        }
+        return res;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+      return atMost(nums,goal)-atMost(nums,goal-1);
     }
 }
