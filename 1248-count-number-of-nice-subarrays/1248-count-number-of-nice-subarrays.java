ef2e7ff5e6dbbcae7a668d490c38cc2f0a673f45
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-04-11 11:39:32 +0530

    Time: 43 ms (21.8%), Space: 56.4 MB (87.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,23 +1,24 @@
 class Solution {
-    int atMost(int []nums,int k){
-        int odds=0;
-        int left=0;
-        int subarrays=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2!=0){
-                odds++;
-            }
-            while(odds>k){
-                if(nums[left]%2!=0){
-                    odds--;
-                }
-                left++;
-            }
-            subarrays+=i-left+1;
-        }
-        return subarrays;
-    }
     public int numberOfSubarrays(int[] nums, int k) {
-       return atMost(nums,k)-atMost(nums,k-1);
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            nums[i]=0;
+        }else{
+            nums[i]=1;
+        }
+       }
+       int sum=0;
+       int res=0;
+       HashMap<Integer,Integer>map=new HashMap<>();
+       map.put(0,1);
+       for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        if(map.containsKey(sum-k)){
+            res+=map.get(sum-k);
+        }
+        map.put(sum,map.getOrDefault(sum,0)+1);
+        
+       }
+       return res;
     }
 }
