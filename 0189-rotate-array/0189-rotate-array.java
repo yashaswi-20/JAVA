64caeaa4dcfec22a5e6fb35b043773efdea6eb1b
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-05-06 12:13:05 +0530

    Time: 2 ms (22.44%), Space: 61.1 MB (83.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,35 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        int j=0;
         int n=nums.length;
-        int start=k%n;
-        start=n-start;
-        int i=0;
-        ArrayList<Integer>list=new ArrayList<>();
-        
-        while(i<n){
-            if(start>=n){
-                int j=start%n;
-                list.add(nums[j]);
-                start++;
-            }else{
-                list.add(nums[start]);
-                start++;
-            }
+        int i=nums.length-1;
+        while(j<i){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            i--;
+            j++;
+        }
+
+        i=0;
+        j=(k%n)-1;
+        while(i<j){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
             i++;
+            j--;
         }
-        for (int j = 0; j < list.size(); j++) {
-            nums[j] = list.get(j);
+        
+        i=k%n;
+        j=n-1;
+        while(i<j){
+            int temp=nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            i++;
+            j--;
         }
+
     }
 }
