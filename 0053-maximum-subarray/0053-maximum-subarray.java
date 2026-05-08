b4824a6247faa576198a34aee43b1c9c14053897
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-05-08 14:47:22 +0530

    Time: 1 ms (99.97%), Space: 77.1 MB (65.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,38 +1,21 @@
 class Solution {
 
-    int cross(int nums[],int start,int end,int mid){
-        int leftMax=Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=mid;i>=start;i--){
-            sum+=nums[i];
-            leftMax=Math.max(leftMax,sum);
-        }
-        sum=0;
-        int rightMax=Integer.MIN_VALUE;
-        for(int i=mid+1;i<=end;i++){
-            sum+=nums[i];
-            rightMax=Math.max(rightMax,sum);
-        }
 
-        return leftMax+rightMax;
-    }
-
-    int  divide(int nums[],int start,int end){
-        if(start==end){
-            return nums[start];
-        }
-        int max=Integer.MIN_VALUE;
-        int mid=start+(end-start)/2;
-       int left= divide(nums,start,mid);
-       int right= divide(nums,mid+1,end);
-       int cross=cross(nums,start,end,mid);
-
-       return Math.max(left,Math.max(right,cross));
-    }
+   
+   
     public int maxSubArray(int[] nums) {
-        int n=nums.length;
+        int sum=nums[0];
+        int max=Integer.MIN_VALUE;
+        max=Math.max(max,sum);
+        for(int i=1;i<nums.length;i++){
+            if(sum>=0){
+            sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
         
-        int res=divide(nums,0,n-1);
-        return res;
     }
 }
