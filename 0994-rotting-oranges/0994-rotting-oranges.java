bbc2850cf94516cc934dc4e04ecddf7a0091b24a
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-05-26 22:43:01 +0530

    Time: 2 ms (90.51%), Space: 44.4 MB (14.71%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,57 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row ,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public int orangesRotting(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        if(n==1 && m==1 && grid[0][0]==0)return 0;
+        Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j));
+                }
+            }
+        }
+        boolean vis[][]=new boolean[grid.length][grid[0].length];
+            int count =0;
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                Pair p=q.poll();
+                int row=p.row;
+                int col=p.col;
+                vis[row][col]=true;
+                
+                int dr[]={1,-1,0,0};
+                int dc[]={0,0,-1,1};
+
+                for(int j=0;j<4;j++){
+                    int tempR=row+dr[j];
+                    int tempC=col + dc[j];
+
+                    if(tempR>=0 && tempR<n && tempC>=0 && tempC<m && grid[tempR][tempC]==1 && vis[tempR][tempC]==false){
+                        q.add(new Pair(tempR,tempC));
+                        grid[tempR][tempC]=2;
+                    }
+                }
+            }
+            count++;
+        }
+         for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return count-1==-1?0 : count-1;
+    }
+}
