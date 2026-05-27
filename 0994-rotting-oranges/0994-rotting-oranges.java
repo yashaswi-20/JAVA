7f30443f051b7b706abfe8dda3e1d54bb77a44f2
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-05-27 16:05:21 +0530

    Time: 76 ms (3.69%), Space: 44.6 MB (9.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,25 +2,33 @@ class Solution {
     class Pair{
         int row;
         int col;
-        Pair(int row ,int col){
+    
+        int  _min;
+        Pair(int row ,int col,int _min){
             this.row=row;
             this.col=col;
+            this._min=_min;
         }
     }
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+        int countFresh=0;
         if(n==1 && m==1 && grid[0][0]==0)return 0;
         Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j));
+                    q.add(new Pair(i,j,0));
+                }
+                if(grid[i][j]==1){
+                    countFresh++;
                 }
             }
         }
  
-            int count =-1;
+           
+        int tm=0;
         while(!q.isEmpty()){
             int size=q.size();
 
@@ -28,7 +36,8 @@ class Solution {
                 Pair p=q.poll();
                 int row=p.row;
                 int col=p.col;
-                
+                int _min=p._min;
+                tm=Math.max(tm,_min);
                 
                 int dr[]={1,-1,0,0};
                 int dc[]={0,0,-1,1};
@@ -38,20 +47,15 @@ class Solution {
                     int tempC=col + dc[j];
 
                     if(tempR>=0 && tempR<n && tempC>=0 && tempC<m && grid[tempR][tempC]==1  ){
-                        q.add(new Pair(tempR,tempC));
+                        q.add(new Pair(tempR,tempC,_min+1));
                         grid[tempR][tempC]=2;
+                        countFresh--;
                     }
                 }
             }
-            count++;
+           
         }
-         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return count==-1?0 : count;
+         if(countFresh!=0)return -1;
+        return tm;
     }
 }
