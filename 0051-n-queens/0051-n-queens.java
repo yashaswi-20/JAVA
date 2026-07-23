ad8d14b8c2205b75403dbbf01be93049e4796d86
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-07-23 08:34:12 +0530

    Time: 3 ms (40.34%), Space: 46.7 MB (46.99%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -42,29 +42,22 @@ class Solution {
         return true;
     }
 
-    void solve(int row, char[][] board) {
+    void solve(int row , char board[][]) {
+        if(row==board.length){
+            ArrayList<String>list=new ArrayList<>();
 
-        if (row == board.length) {
-
-            List<String> temp = new ArrayList<>();
-
-            for (int i = 0; i < board.length; i++) {
-                temp.add(new String(board[i]));
+            for(int i=0;i<board.length;i++){
+                list.add(new String(board[i]));
             }
-
-            res.add(temp);
+            res.add(list);
             return;
         }
 
-        for (int col = 0; col < board.length; col++) {
-
-            if (isSafe(row, col, board)) {
-
-                board[row][col] = 'Q';
-
-                solve(row + 1, board);
-
-                board[row][col] = '.';
+        for(int i=0;i<board.length;i++){
+            if(isSafe(row,i,board)){
+                board[row][i]='Q';
+                solve(row+1,board);
+                board[row][i]='.';
             }
         }
     }
