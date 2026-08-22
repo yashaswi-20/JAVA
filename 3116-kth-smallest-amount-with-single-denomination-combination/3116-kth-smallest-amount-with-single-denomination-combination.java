4df71fe710ce54ddfecab0e5092d145a8a88ab75
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-08-22 13:50:53 +0530

    Time: 29 ms (69.23%), Space: 42.4 MB (100%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -1,103 +1,62 @@
 class Solution {
-
     public long findKthSmallest(int[] coins, int k) {
-
-        long low = 1;
-        long high = (long) coins[0] * k;
-
-        while (low < high) {
-
-            long mid = low + (high - low) / 2;
-
-            long count = countSubsets(
-                0,
-                1,
-                0,
-                mid,
-                coins
-                
-            );
-
-            if (count >= k) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
-
-    private long countSubsets(
-        int index,
-        long lcm,
-        int selected,
-        long x,
-        int[] coins
         
-    ) {
+        long low=1;
+        long high = (long)coins[0]*k;
 
-        // We have considered all coins
-        if (index == coins.length) {
+        while(low<high){
+            long mid= low + (high-low)/2;
 
-            // Empty subset
-            if (selected == 0) {
-                return 0;
+            long count = findCountOfX(0,1,0,mid,coins);
+
+            if(count >= k){
+                high=mid;
+            }else{
+                low=mid+1;
             }
 
-            long count = x / lcm;
+        }
+            return low;
+    }
 
-            if (selected % 2 == 1) {
-                return count;      // ADD
-            } else {
-                return -count;     // SUBTRACT
+        long findCountOfX(int idx, long lcm, long elements ,long x, int[]coins){
+
+            if(idx ==coins.length ){
+                if(elements==0){
+                    return 0;
+                }
+
+                long count=x/lcm;
+                if(elements %2 ==1){
+                    return count;
+                }else{
+                    return -count;
+                }
             }
+
+            long notTake = findCountOfX(idx+1, lcm, elements, x, coins);
+
+            long abc= (long) coins[idx];
+            long newLcm = findLcm(lcm, coins[idx]);
+            long take=0;
+
+            take=findCountOfX(idx+1, newLcm, elements+1, x, coins);
+
+            return take+notTake;
         }
 
-        // Don't take current coin
-        long notTake = countSubsets(
-            index + 1,
-            lcm,
-            selected,
-            x,
-            coins
-            
-        );
-
-        // Take current coin
-        long newLcm = lcm(
-            lcm,
-            coins[index]
-        );
-
-        long take = 0;
-
-        if (newLcm <= x) {
-            take = countSubsets(
-                index + 1,
-                newLcm,
-                selected + 1,
-                x,
-                coins
-                
-            );
+        long findLcm ( long a , long b){
+            return a/ gcd(a,b) * b;
         }
 
-        return notTake + take;
-    }
-
-    private long lcm(long a, long b) {
-        return a / gcd(a, b) * b;
-    }
-
-    private long gcd(long a, long b) {
-
-        while (b != 0) {
-            long temp = a % b;
-            a = b;
-            b = temp;
+        long gcd(long a, long b){
+            while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
         }
-
         return a;
-    }
+        }
+
+    
 }
