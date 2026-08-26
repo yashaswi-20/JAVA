83a07caa2842663eb6ac359f21c1b62d105516c2
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-08-26 18:13:54 +0530

    Time: 19 ms (5.37%), Space: 47.2 MB (6.04%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        String min="";
-        int minLen=Integer.MAX_VALUE;
-        for(int i=0;i<s.length();i++){
-                String temp="";
-                int count=0;
-            for(int j=i;j<s.length();j++){
-                temp+=s.charAt(j);
-                if(s.charAt(j)=='1'){
+        String min = "";
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            String temp = "";
+            int count = 0;
+            for (int j = i; j < s.length(); j++) {
+                temp += s.charAt(j);
+                if (s.charAt(j) == '1') {
                     count++;
                 }
-                if(count==k){
-                   
-                    
-                     if((j-i+1)<minLen){
-                        min=temp;
-                        minLen=temp.length();
-                        //System.out.println(temp + " " + " "+ min +" "+  temp.compareTo(min));
-                        
+                if (count == k) {
+
+                    if ((j - i + 1) < minLen) {
+                        min = temp;
+                        minLen = temp.length();
+
                     }
-                    if((j-i+1)==minLen){
-                        if(temp.compareTo(min)<0){
-                        min=temp;
-                        minLen=temp.length();
-                    }
-                    }
+                    if ((j - i + 1) == minLen) {
+                        if (temp.compareTo(min) < 0) {
+                            min = temp;
+                            minLen = temp.length();
+                        }
+                    }break;
                 }
             }
         }
