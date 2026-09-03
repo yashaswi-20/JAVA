632af3b3d7fef41f912f9da9fbb39faf3f7e36cb
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-09-03 14:13:55 +0530

    Time: 29 ms (15.94%), Space: 118.7 MB (92.03%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -2,14 +2,9 @@ class Solution {
     public boolean uniformArray(int[] nums) {
         Arrays.sort(nums);
 
-        int targetParity = nums[0] % 2;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] % 2 != targetParity) {
-                // Subtract the smallest element
-                int x = nums[i] - nums[0];
-
-                if (x < 1 || x % 2 != targetParity) {
+        if (nums[0] % 2 == 0) {
+            for (int x : nums) {
+                if (x % 2 != 0) {
                     return false;
                 }
             }
