bcd5c709b2a72d3dced1af5739fb9d653348f773
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-09-04 10:59:08 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (49.36%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,25 +1,21 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int i=0;
-        int max=Integer.MIN_VALUE;
-        int stable=Integer.MAX_VALUE;
-        int minStable=Integer.MAX_VALUE;
-        int resIdx=-1;
-        while(i<nums.length){
-        int min=Integer.MAX_VALUE;
-            if(nums[i]>max){
-                max=nums[i];
+        int i = 0;
+        int max = Integer.MIN_VALUE;
+        int stable = Integer.MAX_VALUE;
+        int resIdx = -1;
+        while (i < nums.length) {
+            int min = Integer.MAX_VALUE;
+            if (nums[i] > max) {
+                max = nums[i];
             }
-            for(int j=i;j<nums.length;j++){
-                min=Math.min(nums[j],min);
+            for (int j = i; j < nums.length; j++) {
+                min = Math.min(nums[j], min);
             }
-            //System.out.println(max + " " +min);
-            if(max-min<=k){
-            stable=max-min;
-            }
-            if(stable<minStable){
-                minStable=stable;
-                resIdx=i;
+          
+            if (max - min <= k) {
+                stable = max - min;
+                resIdx = i;
                 break;
             }
 
