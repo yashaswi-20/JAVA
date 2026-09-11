89b9cd7ca297399575e05c65ba5608a1be257b90
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-09-11 18:53:20 +0530

    Time: 5 ms (82.8%), Space: 46.3 MB (46.93%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,36 +1,25 @@
 class Solution {
-    int res = 0;
-    HashSet<String> set = new HashSet<>();
-
-    void solve(int digits[], int j, int vis[], String temp) {
-        //System.out.println(temp);
-
-        if (temp.length() == 3 && temp.charAt(0) != '0' && (temp.charAt(2) - '0') % 2 == 0) {
-            if (!set.contains(temp)) {
-                set.add(temp);
-                res++;
-            }
-            return;
-        }
-
-        for (int i = 0; i < digits.length; i++) {
-            if (temp.length() == 0 && digits[i] == 0)
-                continue;
-
-            if (temp.length() == 2 && digits[i] % 2 != 0)
-                continue;
-            if (vis[i] == 1)
-                continue;
-            vis[i] = 1;
-            solve(digits, i + 1, vis, temp + digits[i]);
-            vis[i] = 0;
-        }
-    }
-
     public int totalNumbers(int[] digits) {
+        Set<Integer> seen = new HashSet<>();
+        int n = digits.length;
 
-        int vis[] = new int[digits.length];
-        solve(digits, 0, vis, "");
-        return res;
+        for (int h = 0; h < n; h++) {
+            if (digits[h] == 0) continue;
+
+            for (int t = 0; t < n; t++) {
+                if (t == h) continue;
+
+                for (int u = 0; u < n; u++) {
+                    if (u == h || u == t) continue;
+
+                    if (digits[u] % 2 != 0) continue;
+
+                    int num = digits[h] * 100 + digits[t] * 10 + digits[u];
+                    seen.add(num);
+                }
+            }
+        }
+
+        return seen.size();
     }
 }
