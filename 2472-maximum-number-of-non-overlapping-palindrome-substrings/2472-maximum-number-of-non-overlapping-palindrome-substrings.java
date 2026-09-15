cc65516bf88ef66572fd84f4078a7793862dccb8
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-09-15 20:37:34 +0530

    Time: 55 ms (47.74%), Space: 43.1 MB (65.83%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,28 +1,45 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
         int n = s.length();
-        if (k == 1)
-            return n;
 
-        int res = 0;
+        // dp[i] = maximum number of palindromes
+        // that can be selected from s[0 ... i-1]
+        int[] dp = new int[n + 1];
 
-        for (int i = 0; i <= n - k; i++) {
-            if (check(s, i, i + k - 1)) {
-                res++;
-                i += k - 1;
-            } else if (i < n - k && check(s, i, i + k)) {
-                res++;
-                i += k;
+        for (int end = 0; end < n; end++) {
+
+            // Don't take a palindrome ending at 'end'
+            dp[end + 1] = dp[end];
+
+            // Try every possible starting point
+            for (int start = end; start >= 0; start--) {
+
+                if (end - start + 1 < k)
+                    continue;
+
+                if (isPalindrome(s, start, end)) {
+
+                    dp[end + 1] = Math.max(
+                        dp[end + 1],
+                        dp[start] + 1
+                    );
+
+                    // Important!
+                    // Since we're processing end from left to right,
+                    // we can stop here for the greedy version.
+                    break;
+                }
             }
         }
 
-        return res;
+        return dp[n];
     }
 
-    boolean check(String s, int l, int r) {
-        for (; l < r; l++, r--)
-            if (s.charAt(l) != s.charAt(r))
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--))
                 return false;
+        }
         return true;
     }
 }
