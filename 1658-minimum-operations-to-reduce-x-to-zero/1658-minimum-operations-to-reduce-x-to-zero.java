9e790e89eeb089b00f0ee7579014ab8676167fb2
Yashaswi <159777502+yashaswi-20@users.noreply.github.com>
2026-09-24 11:06:35 +0530

    Time: 56 ms (13.26%), Space: 145.2 MB (12.29%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,52 +1,38 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
+        int prefix[] = new int[nums.length];
 
-        int n = nums.length;
-
-        int total = 0;
-
-        for (int num : nums) {
-            total += num;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            prefix[i]=sum+nums[i];
+            sum+=nums[i];
         }
+        int target=sum-x;
+        if(target==0) return nums.length;
 
-        int target = total - x;
-
-        // x == total
-        // We have to remove every element
-        if (target == 0) {
-            return n;
-        }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // prefixSum -> earliest index
-        map.put(0, -1);
-
-        int prefixSum = 0;
-        int maxLen = -1;
-
-        for (int i = 0; i < n; i++) {
-
-            prefixSum += nums[i];
-
-            int required = prefixSum - target;
-
-            if (map.containsKey(required)) {
-                int j = map.get(required);
-
-                maxLen = Math.max(maxLen, i - j);
+        HashMap<Integer,Integer>map =new HashMap<>();
+        map.put(0,-1);
+        int count=0;
+        sum=0;
+        int  maxlen=-1;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            map.put(sum,i);
+            
+            if(map.containsKey(sum-target)){
+               int prev=map.get(sum-target);
+                int len=i-prev;
+            maxlen=Math.max(maxlen,len);
             }
-
-            // Keep earliest occurrence
-            if (!map.containsKey(prefixSum)) {
-                map.put(prefixSum, i);
-            }
+           
         }
-
-        if (maxLen == -1) {
+        if (maxlen == -1) {
             return -1;
         }
 
-        return n - maxLen;
-    }
+        return nums.length - maxlen;
+
+
+        
+    }   
 }
